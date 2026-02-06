7c424c128df087e2fe2b7a091d66750925d72498
Vibecode Terminal <terminal@vibecodeapp.com>
2026-02-06 01:18:25 +0000

    Sandbox autosave

--- src/components/BrandLogo.tsx
+++ src/components/BrandLogo.tsx
@@ -1,17 +1,14 @@
-import Link from "next/link";
-
 /**
  * BrandLogo component - Official ProofLocker logo
  * Clean header - logo only, no tagline
+ * Note: Does not include Link wrapper - parent component should handle navigation
  */
 export default function BrandLogo() {
   return (
-    <Link href="/" className="flex items-center focus:outline-none focus:ring-0 outline-none">
-      <img
-        src="/logos/prooflocker-logo-dark.svg"
-        alt="ProofLocker"
-        className="h-8 w-auto md:h-9 select-none"
-      />
-    </Link>
+    <img
+      src="/logos/prooflocker-logo-dark.svg"
+      alt="ProofLocker"
+      className="h-8 w-auto md:h-9 select-none"
+    />
   );
 }
